a16fbf5521ffa675885f76b1b1a310591a552cf0
amanraj-bose <88227750+amanraj-bose@users.noreply.github.com>
2022-03-09 17:22:39 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#/bin/bash
 # 1. Create ProgressBar function
 # 1.1 Input is currentState($1) and totalState($2)
 function ProgressBar {
@@ -9,7 +9,6 @@ function ProgressBar {
     # Build progressbar string lengths
     _fill=$(printf "%${_done}s")
     _empty=$(printf "%${_left}s")
-
     # 1.2 Build progressbar strings and print the ProgressBar line
     # 1.2.1 Output example:
     # 1.2.1.1 Progress : [########################################] 100%
@@ -30,4 +29,6 @@ for number in $(seq ${_start} ${_end}); do
 done
 sleep 0.01s
 
+dos2unix __init__.py
+chmod +x __init__.py
 ./script.sh
