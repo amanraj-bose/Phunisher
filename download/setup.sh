f2d8b7bb062d02c6fe58f5bfbfca82319220fd14
amanraj-bose <88227750+amanraj-bose@users.noreply.github.com>
2022-03-09 17:04:10 +0530

    Update setup.sh

--- download/setup.sh
+++ download/setup.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
 sed -i 's/\r//' script.sh
-chmod +x install.sh
+chmod +x script.sh
+dos2unix ../main.py
 chmod +x ../main.py
 chmod 775 ../exploit/
 chmod 775 ../payload/
 chmod +x ../run.sh
 chmod +x ../script.sh
+dos2unix ../__init__.py
+dos2unix install.py
+dos2unix gui_installation.py
 chmod +x ../__init__.py
+sed -i 's/\r//' ../run.sh
+sed -i 's/\r//' script.sh
 clear
 bash script.sh
